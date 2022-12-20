098c94b7dd960741a8e9c0117c4c63dcf25ca7c3
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-20 10:42:46 +0530

    adding hi.js

---
+++ hi.js
@@ -0,0 +1,4 @@
+a = 20;
+b = 30;
+console.log(a+b);
+console.log('Hello Rohit');
--- new.js
+++
@@ -1,7 +0,0 @@
-// look at the extention *.js
-
-console.log (Hi rohit);
-a = 10;
-b = 20;
-c = 40;
-console.log(a+b+c);
