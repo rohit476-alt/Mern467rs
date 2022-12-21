eb5f3c40cd930072a26af416c023994f0a971293
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-21 09:25:09 +0530

    Create datatype.js
    datatype.js adding

---
+++ datatype.js
@@ -0,0 +1,15 @@
+let a = 20
+a = 34.78
+a = 'abc'
+
+const name ='abc'//string
+const age = 20 //numeric
+const iskoay = true // boolean
+const date = new date () //class object
+const obj = { // literal obj
+    sin : 67
+
+}
+const cr = null // refers to null value 
+const speed = undefined
+const abc = NaN
