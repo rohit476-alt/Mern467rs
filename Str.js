8e9bceb7717c2cf3f779bfdc0faaa6710d815789
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-29 10:43:55 +0530

    js.str

---
+++ Str.js
@@ -0,0 +1,12 @@
+const nm = 'abc,pqe,imn'//[a,b,c]
+const carNm = 'pqr'
+
+    console.log(nm);
+    console.log(nm. touppercase ())
+    console.log(nm. tolowrcase ())
+    console.log(CarNm. repeat(3))
+    console.log(nm.charAt (3))
+    console.log(CarNm. split(''))
+    console.log(nm. substring (1,2))
+    
+    
