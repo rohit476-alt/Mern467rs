6129aa45988c625c8bd33d1582564ab2ef146b7b
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-29 17:51:22 +0530

    datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+let a = 20
+a = 34.78
+a = 'abc'
+
+
+   const name = 'abc'
+   const age = 20
+   const isokay = true
+   const dt = new DataTransfer()
+   const obj = {
+    sin : 67
+   }
+     const cr = null
+     const speed = undefined
+     const abc = NaN
+     //function
+
+      const fn = function(){
+
+      }
+  
