559a218d7087ff62ea9d7c1ce3fe061c51986221
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-29 10:34:05 +0530

    js.variable

---
+++ variable.js
@@ -0,0 +1,30 @@
+X = 89 //x bad way of defining variable
+var a = 20// older js -> convert to let
+let b = 30// es6 -> scoping is considered
+const c = 50 // es6
+// c = 90 // not possible.
+
+
+let z = 90
+let Z = 90
+if(true) {
+    let z = 89
+    z = 78
+}
+console.log(z)
+
+console.log(z)
+
+// Names can contaion letters, digits underscores, and dollar signs.
+// Names must begin with a letter.
+// Names can also being with $ and  -
+// Names are case sensitivite.
+// Reserved words (like javasript keywords) cannot be used as names.
+
+const _123456 = 10
+const fn = 90 
+ 
+  // you practice -> don't upload to git -> you did't practice 
+  // you peactice -> uploaded to git -> you actually practice
+
+
