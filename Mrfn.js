c419a358935f3a59a6b9e4e40fa5c81392c213e4
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-29 11:16:32 +0530

    Mrfn.js

---
+++ Mrfn.js
@@ -0,0 +1,16 @@
+function nmd (nm){
+    return '#' + nm. touppercase ()+ '$'
+
+}
+    const anFn = function (nm){
+        return '#' + nm. touppercase ()+ '$'
+
+        }
+        const cal = 10
+         
+        console.log(nmd('abc'))
+        console.log(anFn('pqr'))
+        console.log(anFn)
+        console.log(cal)
+         
+        //Es6
