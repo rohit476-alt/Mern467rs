530d35237beb738eb782fbb964b98cf96084d1e9
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2022-12-29 18:36:07 +0530

    bltfn.js

---
+++ bltfn.js
@@ -0,0 +1,16 @@
+console.log('Hey Rohit');
+
+     const a = '10'
+     const num1 = Number .parseInt(a);
+     const b = '20'
+     const num2 = Number .parseInt(b);
+     console.log (a+b);
+     console.log(num1+num2);
+
+       const isOkay = true
+       console.log(''+ isOkay);
+       console.log(isOkay.toString());
+       console.log((9337334356).toString());
+
+
+
