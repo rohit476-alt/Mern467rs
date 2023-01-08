1e84d5e48baaadb5d80b52c21fd2a214f65644f1
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2023-01-08 12:42:32 +0530

    add all code.js

---
+++ scope.js
@@ -0,0 +1,16 @@
+Url = 'local'
+const cfg = 'pqr'
+
+   cstupr ()
+   function cstupr (){
+    const nm = 'abc'
+    console.log(this)
+    console.log ('Inside of funtion :'+ nm)
+   }
+//console.log('outsode of function;'+ nm)
+var i = 10
+if (true){
+       var i = 20
+
+}
+    console.log (i)
---
+++ strinterploation.js
@@ -0,0 +1,10 @@
+const fnNm = 'abc'
+
+const lsNm = 'pqr'
+const flNm1 = 'This is Rohit'+fnNm + ' ' + lsNm
+console.log(flNm1)
+
+// es6 - string interploation
+const flNm2 = `This is Rohit ${fnNm} ${lsNm}`
+const res1 = `Addition ${10 + 10}`
+const res2 = `Uppercase ${fnNm.toUpperCase()}`
