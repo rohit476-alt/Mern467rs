4872012dc7b6da01651f9174013c39c19736e96e
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2023-02-28 14:52:05 +0530

    project Responsive resume

---
+++ Project1/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    $('#menu').click(function(){
+        $(this).toggleclass('fa-times');
+        $('header').toggleclass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeclass('fa-times');
+        $('header').removeclass('toggle');
+
+    });
+
+});
