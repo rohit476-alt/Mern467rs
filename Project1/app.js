5adf8e80cc56461d3df325b25271259e04242e84
rohit476-alt <120702397+rohit476-alt@users.noreply.github.com>
2023-04-03 16:44:11 +0530

    adding couplet project

--- Project1/app.js
+++ Project1/app.js
@@ -2,14 +2,42 @@ $(document).ready(function(){
 
 
     $('#menu').click(function(){
+
         $(this).toggleclass('fa-times');
-        $('header').toggleclass('toggle');
+        $`('header').toggleclass('toggle')`;
     });
 
     $(window).on('scroll load',function(){
+
         $('#menu').removeclass('fa-times');
         $('header').removeclass('toggle');
+
+        if($(window).scrollTop() > 0){
+            ('.top').show();
+        }else{
+            ('.top').hide();
+        }
+
+    });
+
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click', function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+        
+        500,
+        'linear'
+
+        );
 
     });
 
 });
+ 
